51b6f3279a3ae0f394ab71e13e34b5b4c59da544
Azeem Adenuga <adenugaazeem80@gmail.com>
2023-10-07 10:43:21 +0100

    changes to layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="icon"
-          href="/images/logo/cinqunecinq_logo.png"
-          type="image/x-icon"
-          sizes="16x16"
-        />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
